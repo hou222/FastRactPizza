b5adc481a8780dd4654add3c67482ade97bed5a3
Houssam Benlagha <houssambenlagha0@gmail.com>
2024-01-27 19:35:36 +0100

    adding the priority feature

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,8 +1,13 @@
+import { useSelector } from 'react-redux';
 import { formatCurrency } from '../../utils/helpers';
 import DeleteItem from './DeleteItem';
+import UpdateItemQuantity from './UpdateItemQuantity';
+import { getCurrentQuantityById } from './cartSlice';
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
+
+  const currentQuantity = useSelector(getCurrentQuantityById(pizzaId));
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
@@ -15,6 +20,7 @@ function CartItem({ item }) {
       >
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
       </div>
+      <UpdateItemQuantity pizzaId={pizzaId} currentQuantity={currentQuantity} />
       <DeleteItem pizzaId={pizzaId} />
     </li>
   );
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -37,8 +37,10 @@ const cartSlice = createSlice({
       // payload = pizzaId
       const item = state.cart.find((item) => item.pizzaId === action.payload);
 
-      item.quantity++;
+      item.quantity--;
       item.totalPrice = item.quantity * item.unitPrice;
+
+      if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
     },
     clearCart(state) {
       state.cart = [];
